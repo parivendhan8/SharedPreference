7b9e44d5d71aaf8f95e1a150275c43262ee3e0ea
parivendhan8 <parivendhan8@gmail.com>
2019-05-16 16:20:24 +0530

    Preference

--- app/src/main/java/com/example/sharedpreference/List.java
+++ app/src/main/java/com/example/sharedpreference/List.java
@@ -14,6 +14,8 @@ public class List extends AppCompatActivity {
     private int listViewTouchAction = -1;
     private boolean set = false;
 
+    /*Pari*/
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
